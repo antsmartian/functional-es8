8c509db24659d1be59661d44255be41f8fd81f1c
anto <anto.aravinth.cse@gmail.com>
2017-02-24 14:31:26 +0530

    Merging the changes to a new branch

--- functional-playground/play.js
+++ functional-playground/play.js
@@ -1,143 +1,142 @@
 import {Container,MayBe,arrayUtils,Some,Nothing} from '../lib/es6-functional.js'
 
-var request = require('sync-request');
+let https = require('https');
 
-
-let searchReddit = (search) => {
-    let response  
-    try{
-       response = JSON.parse(request('GET',"https://www.reddit.com/search.json?q=" + encodeURI(search) + "&limit=2").getBody('utf8'))
-    }catch(err) {
-        response = { message: "Something went wrong" , errorCode: err['statusCode'] }
-    }
-    return response
+function* gen() {
+    return 'first generator';
 }
 
-let getComments = (link) => {
-    let response
-    try {
-        console.log("https://www.reddit.com/" + link)
-        response = JSON.parse(request('GET',"https://www.reddit.com/" + link).getBody('utf8'))
-    } catch(err) {
-        console.log(err)
-        response = { message: "Something went wrong" , errorCode: err['statusCode'] }
-    }
+let generatorResult = gen()
 
-    return response 
+console.log("For first time",generatorResult.next().value)
+console.log("For second time time",generatorResult.next().value)
+
+let generatorResult2 = gen()
+generatorResult = gen() //re-create again
+
+console.log("First generator",generatorResult.next().value)
+console.log("Second generator",generatorResult2.next().value)
+
+function* generatorSequence() {
+    yield 'first';
+    yield 'second';
+    yield 'third';
 }
 
-let mergeTitleAndComments = (searchMayBe,commentsMayBe) => {
-    searchMayBe.map((value) => {
-        commentsMayBe.map((commentsValue) => {
-            console.log(value)
-            console.log(commentsValue)
-        })
+let generatorSequenceResult = generatorSequence();
+
+console.log("\n\n\n")
+console.log('First time sequence value',generatorSequenceResult.next().value)
+console.log('Second time sequence value',generatorSequenceResult.next().value)
+console.log('thrid time sequence value',generatorSequenceResult.next().value)
+
+
+generatorSequenceResult = generatorSequence();
+
+console.log("\n\n")
+console.log('done value for the first time',generatorSequenceResult.next())
+console.log('done value for the second time',generatorSequenceResult.next())
+console.log('done value for the third time',generatorSequenceResult.next())
+console.log('done value for the fourth time',generatorSequenceResult.next())
+
+console.log("\n\n")
+for(let value of generatorSequence())
+    console.log("for of value of generatorSequence is",value)
+
+
+//passing data to generators
+function* sayFullName() {
+    var firstName = yield 
+    var secondName = yield
+    console.log(firstName + secondName);
+}
+
+let fullName = sayFullName()
+fullName.next()
+fullName.next('anto')
+fullName.next('aravinth')
+
+let getDataOne = (cb) => {
+    setTimeout(function(){
+        //calling the callback
+        cb('dummy data one')
+    }, 1000);
+}
+
+let getDataTwo = (cb) => {
+    setTimeout(function(){
+        //calling the callback
+        cb('dummy data two')
+    }, 1000);
+}
+
+getDataOne((data) => console.log("data received",data))
+getDataTwo((data) => console.log("data received",data))
+
+//using generator
+let generator;
+getDataOne = () => {
+    setTimeout(function(){
+        //call the generator and
+        //pass data via `next`
+        generator.next('dummy data one')
+    }, 1000);
+}
+
+getDataTwo = () => {
+    setTimeout(function(){
+        //call the generator and
+        //pass data via `next`
+        generator.next('dummy data two')
+    }, 1000);
+}
+
+function* main() {
+    let dataOne = yield getDataOne();
+    let dataTwo = yield getDataTwo();
+    console.log("data one",dataOne)
+    console.log("data two",dataTwo)
+}
+
+generator = main()
+generator.next()
+
+
+function httpGetAsync(url,callback) {
+
+    return https.get(url, 
+        function(response) {
+            var body = '';
+            response.on('data', function(d) {
+                body += d;
+            });
+            response.on('end', function() {
+                let parsed = JSON.parse(body)
+                callback(parsed)
+            })
+        }
+    );
+
+}
+
+httpGetAsync('https://www.reddit.com/r/pics/.json',(picJson)=> {
+    httpGetAsync(picJson.data.children[0].data.url+".json",(firstPicRedditData) => {
+        console.log(firstPicRedditData)
     })
-}
-
-let mergeViaMayBe = (searchText) => {
-    let redditMayBe = MayBe.of(searchReddit(searchText))
-    let ans = redditMayBe
-               .map((arr) => arr['data'])
-               .map((arr) => arr['children'])
-               .map((arr) => arrayUtils.map(arr,(x) => {
-                        return {
-                            title : x['data'].title,
-                            permalink : x['data'].permalink
-                        }
-                    } 
-                ))
-               .map((obj) => arrayUtils.map(obj, (x) => {
-                    return {
-                        title: x.title,
-                       comments: MayBe.of(getComments(x.permalink.replace("?ref=search_posts",".json")))
-                    }
-               }))
-
-   return ans;
-}
-
-
-let mergeViaJoin = (searchText) => {
-    let redditMayBe = MayBe.of(searchReddit(searchText))
-    let ans = redditMayBe.map((arr) => arr['data'])
-               .map((arr) => arr['children'])
-               .map((arr) => arrayUtils.map(arr,(x) => {
-                        return {
-                            title : x['data'].title,
-                            permalink : x['data'].permalink
-                        }
-                    } 
-                ))
-               .map((obj) => arrayUtils.map(obj, (x) => {
-                    return {
-                        title: x.title,
-                       comments: MayBe.of(getComments(x.permalink.replace("?ref=search_posts",".json"))).join()
-                    }
-               }))
-               .join()
-
-   return ans;
-}
-
-let answer;
-answer = mergeViaMayBe("functional programming")
-
-console.log(answer)
-
-// Via MayBe functor 
-/*
-    See how deep we map to get the answers
-*/
-answer.map((result) => {
-    arrayUtils.map(result,(mergeResults) => {
-        mergeResults.comments.map(comment => {
-            console.log(comment)
-        })
-    }) 
-})
-
-//simple examples of join
-let joinExample = MayBe.of(MayBe.of(5))
-console.log("Without Join Example",joinExample.map((outsideMayBe) => {
-    return outsideMayBe.map((x) => x + 4)
-}))
-console.log("Join Example",joinExample.join().map((v) => v + 4))
-
-//trying our old problem with join
-answer = mergeViaJoin("functional programming")
-
-console.log(answer)
-
-arrayUtils.map(answer,a => {
-    console.log(a.comments)
 })
 
 
-let mergeViaChain = (searchText) => {
-    let redditMayBe = MayBe.of(searchReddit(searchText))
-    let ans = redditMayBe.map((arr) => arr['data'])
-               .map((arr) => arr['children'])
-               .map((arr) => arrayUtils.map(arr,(x) => {
-                        return {
-                            title : x['data'].title,
-                            permalink : x['data'].permalink
-                        }
-                    } 
-                ))
-               .chain((obj) => arrayUtils.map(obj, (x) => {
-                    return {
-                       title: x.title,
-                       comments: MayBe.of(getComments(x.permalink.replace("?ref=search_posts",".json"))).chain(x => {
-                            return x.length
-                       })
-                    }
-               }))
-
-   return ans;
+function request(url) {
+    httpGetAsync( url, function(response){
+        generator.next( response );
+    } );
 }
 
-//trying our old problem with chain
-answer = mergeViaChain("functional programming")
+function *main() {
+    let picturesJson = yield request( "https://www.reddit.com/r/pics/.json" );
+    let firstPictureData = yield request(picturesJson.data.children[0].data.url+".json")
+    console.log(firstPictureData)
+}
 
-console.log(answer)
+generator = main()
+generator.next()
