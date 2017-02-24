ef2b0d33e446cb15981a56c08a75b0f801f8f89c
anto <anto.aravinth.cse@gmail.com>
2017-02-24 14:27:48 +0530

    Merging the changes to a new branch

--- functional-playground/play.js
+++ functional-playground/play.js
@@ -1,224 +1,92 @@
-import {arrayUtils} from '../lib/es6-functional.js'
+import {curry,curryN,partial} from '../lib/es6-functional.js'
 
-const map = arrayUtils.map;
-const filter = arrayUtils.filter;
-const concatAll = arrayUtils.concatAll;
-const reduce = arrayUtils.reduce;
-const zip = arrayUtils.zip;
+const add = (x,y) => x + y;
+let autoCurriedAdd = curry(add)
+console.log("Curried summation",autoCurriedAdd(2)(2))
 
-let squaredArray = map([1,2,3], (x) => x * x)
-console.log(squaredArray)
+const genericTable = (x,y) => x * y
 
-let apressBooks = [
-  {
-    "id": 111,
-    "title": "C# 6.0",
-    "author": "ANDREW TROELSEN",
-    "rating": [4.7],
-    "reviews": [{good : 4 , excellent : 12}]
-  },
-  {
-    "id": 222,
-    "title": "Efficient Learning Machines",
-    "author": "Rahul Khanna",
-    "rating": [4.5],
-    "reviews": []
-  },
-  {
-    "id": 333,
-    "title": "Pro AngularJS",
-    "author": "Adam Freeman",
-    "rating": [4.0],
-    "reviews": []
-  },
-  {
-    "id": 444,
-    "title": "Pro ASP.NET",
-    "author": "Adam Freeman",
-    "rating": [4.2],
-    "reviews": [{good : 14 , excellent : 12}]
-  }
-];
+const tableOf2 = curry(genericTable)(2)
+const tableOf3 = curry(genericTable)(3)
+const tableOf4 = curry(genericTable)(4)
 
-//get only title and author fields
-let resultOfTitleName = map(apressBooks,(book) => {
-  return {title: book.title,author:book.author}
-})
+console.log("Table via currying")
+console.log("2 * 2 =",tableOf2(2))
+console.log("2 * 3 =",tableOf2(3))
+console.log("2 * 4 =",tableOf2(4))
 
-console.log(resultOfTitleName)
+console.log("3 * 2 =",tableOf3(2))
+console.log("3 * 3 =",tableOf3(3))
+console.log("3 * 4 =",tableOf3(4))
 
-//get only book with above rating 4.5s
-let filteredArray = filter(apressBooks, (book) => book.rating[0] > 4.5)
+console.log("4 * 2 =",tableOf4(2))
+console.log("4 * 3 =",tableOf4(3))
+console.log("4 * 4 =",tableOf4(4))
 
-console.log(filteredArray)
+//There is no console.log in node world :) 
+//So you can run the code by copying paste in browser!
+/*
+const loggerHelper = (mode,initialMessage,errorMessage,lineNo) => {
+  if(mode === "DEBUG")
+    console.debug(initialMessage,errorMessage + "at line: " + lineNo)
+  else if(mode === "ERROR")
+    console.error(initialMessage,errorMessage + "at line: " + lineNo)
+  else if(mode === "WARN")
+    console.warn(initialMessage,errorMessage + "at line: " + lineNo)
+  else 
+    throw "Wrong mode"
+}
 
-//get title and author whose rating is above 4.5
-let goodRatingBooks =
- filter(apressBooks, (book) => book.rating[0] > 4.5)
+let errorLogger = curryN(loggerHelper)("ERROR")("Error At Stats.js");
+let debugLogger = curryN(loggerHelper)("DEBUG")("Debug At Stats.js");
+let warnLogger = curryN(loggerHelper)("WARN")("Warn At Stats.js");
 
-console.log("Good Rated books",map(goodRatingBooks,(book) => {
-  return {title: book.title,author:book.author}
-}))
 
-//modified data structure.
-let apressBooks2 = [
-  {
-    name : "beginners",
-    bookDetails : [
-      {
-        "id": 111,
-        "title": "C# 6.0",
-        "author": "ANDREW TROELSEN",
-        "rating": [4.7],
-        "reviews": [{good : 4 , excellent : 12}]
-      },
-      {
-        "id": 222,
-        "title": "Efficient Learning Machines",
-        "author": "Rahul Khanna",
-        "rating": [4.5],
-        "reviews": []
-      }
-    ]
-  },
-  {
-      name : "pro",
-      bookDetails : [
-      {
-        "id": 333,
-        "title": "Pro AngularJS",
-        "author": "Adam Freeman",
-        "rating": [4.0],
-        "reviews": []
-      },
-      {
-        "id": 444,
-        "title": "Pro ASP.NET",
-        "author": "Adam Freeman",
-        "rating": [4.2],
-        "reviews": [{good : 14 , excellent : 12}]
-      }
-    ]
-  }
-];
+//for error
+errorLogger("Error message",21)
 
-console.log('Mapped new data structure',
-map(apressBooks2,(book) => {
-  return book.bookDetails
-}))
+//for debug
+debugLogger("Debug message",233)
 
-console.log('Flattend Array',
-concatAll(
-  map(apressBooks2,(book) => {
-    return book.bookDetails
-  })
-))
+//for warn
+warnLogger("Warn message",34)
+*/
 
-//result of our problem
-let goodRatingCriteria = (book) => book.rating[0] > 4.5;
-console.log("Result using map,filter,concatAll",
-filter(
-  concatAll(
-    map(apressBooks2,(book) => {
-      return book.bookDetails
-    })
-  )
-,goodRatingCriteria))
+let match = curryN(function(expr, str) {
+  return str.match(expr);
+});
 
-console.log("Sum of the array",
-reduce([1,2,3,4,5],(acc,val) => acc + val,0))
+let hasNumber = match(/[0-9]+/)
 
-console.log("Product of the array",
-reduce([1,2,3,4,5],(acc,val) => acc * val,1))
+let filter = curryN(function(f, ary) {
+  return ary.filter(f);
+});
 
-let bookDetails = concatAll(
-  map(apressBooks2,(book) => {
-    return book.bookDetails
-  })
-)
+let findNumbersInArray = filter(hasNumber)
+console.log("Finding numbers via curry",findNumbersInArray(["js","number1"]))
 
-let resultOfCountReviews = reduce(bookDetails,(acc,bookDetail) => {
-  let goodReviews = bookDetail.reviews[0] != undefined ? bookDetail.reviews[0].good : 0
-  let excellentReviews = bookDetail.reviews[0] != undefined ? bookDetail.reviews[0].excellent : 0
-  return {good: acc.good + goodReviews,excellent : acc.excellent + excellentReviews}
-},{good:0,excellent:0})
+let map = curry(function(f, ary) {
+  return ary.map(f);
+});
 
-console.log("Good and Excellent count",resultOfCountReviews)
+let squareAll = map((x) => x * x)
 
-console.log("Addition of two arrays using zip",
-zip([1,2,3],[4,5,6],(x,y) => x+y))
+console.log("Squaring the array with currying",squareAll([1,2,3]))
 
-let apressBooks3 = [
-  {
-    name : "beginners",
-    bookDetails : [
-      {
-        "id": 111,
-        "title": "C# 6.0",
-        "author": "ANDREW TROELSEN",
-        "rating": [4.7]
-      },
-      {
-        "id": 222,
-        "title": "Efficient Learning Machines",
-        "author": "Rahul Khanna",
-        "rating": [4.5],
-        "reviews": []
-      }
-    ]
-  },
-  {
-      name : "pro",
-      bookDetails : [
-      {
-        "id": 333,
-        "title": "Pro AngularJS",
-        "author": "Adam Freeman",
-        "rating": [4.0],
-        "reviews": []
-      },
-      {
-        "id": 444,
-        "title": "Pro ASP.NET",
-        "author": "Adam Freeman",
-        "rating": [4.2]
-      }
-    ]
-  }
-];
+setTimeout(() => console.log("Print after 10 ms."),10);
 
-let reviewDetails = [
-  {
-    "id": 111,
-    "reviews": [{good : 4 , excellent : 12}]
-  },
-  {
-    "id" : 222,
-    "reviews" : []
-  },
-  {
-    "id" : 333,
-    "reviews" : []
-  },
-  {
-    "id" : 444,
-    "reviews": [{good : 14 , excellent : 12}]
-  }
-]
+const setTimeoutWrapper = (time,fn) => {
+  setTimeout(fn,time);
+}
+//using curring
+const delayTenMs = curryN(setTimeoutWrapper)(10)
+delayTenMs(() => console.log("Do X task"))
+delayTenMs(() => console.log("Do Y task"))
 
-let bookDetails3 = concatAll(
-  map(apressBooks3,(book) => {
-    return book.bookDetails
-  })
-)
+//using partial application
+let delayTenMsPartial = partial(setTimeout,undefined,10);
+delayTenMsPartial(() => console.log("Do X. . .  task"))
+delayTenMsPartial(() => console.log("Do Y . . . . task"))
 
-let mergedBookDetails = zip(bookDetails3,reviewDetails,(book,review) => {
-  if(book.id === review.id)
-  {
-    let clone = Object.assign({},book)
-    clone.ratings = review
-    return clone
-  }
-})
-
-console.log("Merged details",mergedBookDetails)
+let prettyPrintJson = partial(JSON.stringify,undefined,null,2)
+console.log("JSON pretty print via partial",prettyPrintJson({foo: "bar", bar: "foo"}))
