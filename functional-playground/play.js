1f5b8de0eb8a5db1c90e05a605238260182c7744
anto <anto.aravinth.cse@gmail.com>
2017-02-24 14:30:39 +0530

    Merging the changes to a new branch

--- functional-playground/play.js
+++ functional-playground/play.js
@@ -1,101 +1,143 @@
 import {Container,MayBe,arrayUtils,Some,Nothing} from '../lib/es6-functional.js'
+
 var request = require('sync-request');
 
-//using bare new keyword
-console.log("Container using bare new keyword")
-let testValue = new Container(3)
-console.log("Value inside container",testValue)
 
-let testObj = new Container({a:1})
-console.log("Object inside container",testObj)
-
-let testArray = new Container([1,2])
-console.log("Array inside container",testArray)
-
-//using `of` method
-console.log("\n\nContainer using `of` util method")
-testValue = Container.of(3)
-console.log("Value inside container",testValue)
-
-testObj = Container.of({a:1})
-console.log("Object inside container",testObj)
-
-testArray = Container.of([1,2])
-console.log("Array inside container",testArray)
-
-console.log("Nested conatiner",Container.of(Container.of(3)))
-
-let double = (x) => x + x;
-console.log("Double container",Container.of(3).map(double));
-
-
-console.log("May Be Example",MayBe.of("string").map((x) => x.toUpperCase()))
-console.log("May Be null example",MayBe.of(null).map((x) => x.toUpperCase()))
-console.log("MayBe chaining",MayBe.of("George")
-     .map((x) => x.toUpperCase())
-     .map((x) => "Mr. " + x))
-
-console.log("MayBe chaining null",
-    MayBe.of("George")
-     .map(() => undefined)
-     .map((x) => "Mr. " + x))
-
-let getTopTenSubRedditPosts = (type) => {
-
+let searchReddit = (search) => {
     let response  
     try{
-       response = JSON.parse(request('GET',"https://www.reddit.com/r/subreddits/" + type + ".json?limit=10").getBody('utf8'))
+       response = JSON.parse(request('GET',"https://www.reddit.com/search.json?q=" + encodeURI(search) + "&limit=2").getBody('utf8'))
     }catch(err) {
         response = { message: "Something went wrong" , errorCode: err['statusCode'] }
     }
     return response
 }
 
-let getTopTenSubRedditData = (type) => {
-    let response = getTopTenSubRedditPosts(type);
-    return MayBe.of(response).map((arr) => arr['data'])
-                             .map((arr) => arr['children'])
-                             .map((arr) => arrayUtils.map(arr,
-                                (x) => { 
-                                    return {
-                                        title : x['data'].title,
-                                        url   : x['data'].url
-                                    } 
-                                }
-                            ))
-}
-
-console.log("Proper reddit type",getTopTenSubRedditData('new'))
-console.log("Wrong reddit type",getTopTenSubRedditData('neww'))
-
-console.log("\nEither example\n")
-console.log("Something example", Some.of("test").map((x) => x.toUpperCase()))
-console.log("Nothing example", Nothing.of("test").map((x) => x.toUpperCase()))
-
-let getTopTenSubRedditPostsEither = (type) => {
-
-    let response  
-    try{
-       response = Some.of(JSON.parse(request('GET',"https://www.reddit.com/r/subreddits/" + type + ".json?limit=10").getBody('utf8')))
-    }catch(err) {
-        response = Nothing.of({ message: "Something went wrong" , errorCode: err['statusCode'] })
+let getComments = (link) => {
+    let response
+    try {
+        console.log("https://www.reddit.com/" + link)
+        response = JSON.parse(request('GET',"https://www.reddit.com/" + link).getBody('utf8'))
+    } catch(err) {
+        console.log(err)
+        response = { message: "Something went wrong" , errorCode: err['statusCode'] }
     }
-    return response
+
+    return response 
 }
 
-let getTopTenSubRedditDataEither = (type) => {
-    let response = getTopTenSubRedditPostsEither(type);
-    return response.map((arr) => arr['data'])
-                             .map((arr) => arr['children'])
-                             .map((arr) => arrayUtils.map(arr,
-                                (x) => { 
-                                    return {
-                                        title : x['data'].title,
-                                        url   : x['data'].url
-                                    } 
-                                }
-                            ))
+let mergeTitleAndComments = (searchMayBe,commentsMayBe) => {
+    searchMayBe.map((value) => {
+        commentsMayBe.map((commentsValue) => {
+            console.log(value)
+            console.log(commentsValue)
+        })
+    })
 }
 
-console.log("Correct reddit type ",getTopTenSubRedditDataEither('new'))
-console.log("Wrong reddit type ",getTopTenSubRedditDataEither('new2'))
+let mergeViaMayBe = (searchText) => {
+    let redditMayBe = MayBe.of(searchReddit(searchText))
+    let ans = redditMayBe
+               .map((arr) => arr['data'])
+               .map((arr) => arr['children'])
+               .map((arr) => arrayUtils.map(arr,(x) => {
+                        return {
+                            title : x['data'].title,
+                            permalink : x['data'].permalink
+                        }
+                    } 
+                ))
+               .map((obj) => arrayUtils.map(obj, (x) => {
+                    return {
+                        title: x.title,
+                       comments: MayBe.of(getComments(x.permalink.replace("?ref=search_posts",".json")))
+                    }
+               }))
+
+   return ans;
+}
+
+
+let mergeViaJoin = (searchText) => {
+    let redditMayBe = MayBe.of(searchReddit(searchText))
+    let ans = redditMayBe.map((arr) => arr['data'])
+               .map((arr) => arr['children'])
+               .map((arr) => arrayUtils.map(arr,(x) => {
+                        return {
+                            title : x['data'].title,
+                            permalink : x['data'].permalink
+                        }
+                    } 
+                ))
+               .map((obj) => arrayUtils.map(obj, (x) => {
+                    return {
+                        title: x.title,
+                       comments: MayBe.of(getComments(x.permalink.replace("?ref=search_posts",".json"))).join()
+                    }
+               }))
+               .join()
+
+   return ans;
+}
+
+let answer;
+answer = mergeViaMayBe("functional programming")
+
+console.log(answer)
+
+// Via MayBe functor 
+/*
+    See how deep we map to get the answers
+*/
+answer.map((result) => {
+    arrayUtils.map(result,(mergeResults) => {
+        mergeResults.comments.map(comment => {
+            console.log(comment)
+        })
+    }) 
+})
+
+//simple examples of join
+let joinExample = MayBe.of(MayBe.of(5))
+console.log("Without Join Example",joinExample.map((outsideMayBe) => {
+    return outsideMayBe.map((x) => x + 4)
+}))
+console.log("Join Example",joinExample.join().map((v) => v + 4))
+
+//trying our old problem with join
+answer = mergeViaJoin("functional programming")
+
+console.log(answer)
+
+arrayUtils.map(answer,a => {
+    console.log(a.comments)
+})
+
+
+let mergeViaChain = (searchText) => {
+    let redditMayBe = MayBe.of(searchReddit(searchText))
+    let ans = redditMayBe.map((arr) => arr['data'])
+               .map((arr) => arr['children'])
+               .map((arr) => arrayUtils.map(arr,(x) => {
+                        return {
+                            title : x['data'].title,
+                            permalink : x['data'].permalink
+                        }
+                    } 
+                ))
+               .chain((obj) => arrayUtils.map(obj, (x) => {
+                    return {
+                       title: x.title,
+                       comments: MayBe.of(getComments(x.permalink.replace("?ref=search_posts",".json"))).chain(x => {
+                            return x.length
+                       })
+                    }
+               }))
+
+   return ans;
+}
+
+//trying our old problem with chain
+answer = mergeViaChain("functional programming")
+
+console.log(answer)
