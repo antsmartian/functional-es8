40f25dd35ff74ace0a6e95df8aff47bd7a2b5ae7
vishwanathsrikanth <vism@microsoft.com>
2018-05-03 01:05:52 +0530

    jasmine and mocha tests added

--- functional-playground/play.js
+++ functional-playground/play.js
@@ -1,7 +1,13 @@
-import forEach from '../lib/es6-functional.js'
+import { forEach, Sum, fetchTextByPromise } from '../lib/es6-functional.js'
 
 var array = [1,2,3]
+
 //print the console
-forEach(array,(data) => console.log(data))
-//double the contents
-forEach(array,(data) => console.log(2 * data))
+console.log(Sum(array))
+
+let sayHello = async () => {
+    const externalFetchedText = await fetchTextByPromise();
+    console.log(`Response from SayHello: Hello, ${externalFetchedText}`); // Hello, es8
+}
+
+sayHello()
